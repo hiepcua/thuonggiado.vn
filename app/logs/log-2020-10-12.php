11ccef4ac56ad3250bfd4f75f5798b48cd0996dd
Tran Hiep <tranviethiepdz@gmail.com>
2020-10-12 19:14:23 +0700

    update

---
+++ app/logs/log-2020-10-12.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-10-12 08:44:13 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'thuonggiado.vn' D:\xampp\htdocs\thuonggiado.vn\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-12 08:44:13 --> Unable to connect to the database
+ERROR - 2020-10-12 08:44:13 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'thuonggiado.vn' D:\xampp\htdocs\thuonggiado.vn\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-12 08:44:13 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: D:\xampp\tmp) D:\xampp\htdocs\thuonggiado.vn\system\libraries\Session\Session.php 143
+ERROR - 2020-10-12 08:44:13 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'thuonggiado.vn' D:\xampp\htdocs\thuonggiado.vn\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-12 08:44:13 --> Unable to connect to the database
+ERROR - 2020-10-12 08:44:13 --> Query error: Unknown database 'thuonggiado.vn' - Invalid query: SET SESSION sql_mode = ""
+ERROR - 2020-10-12 08:44:13 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'thuonggiado.vn' D:\xampp\htdocs\thuonggiado.vn\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2020-10-12 08:44:13 --> Unable to connect to the database
+ERROR - 2020-10-12 08:44:13 --> Query error: Unknown database 'thuonggiado.vn' - Invalid query: SELECT *
+FROM `sma_settings`
+ERROR - 2020-10-12 08:44:13 --> Severity: error --> Exception: Call to a member function num_rows() on boolean D:\xampp\htdocs\thuonggiado.vn\app\models\Site.php 305
